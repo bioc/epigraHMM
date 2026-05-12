144e3bbd0893eafe6783aeceab95e171251b447f
Pedro Baldoni <pedrobaldoni@gmail.com>
2026-05-13 09:37:46 +1000

    Fix plotPatterns function to handle case with a single genomic window/row. Now using 500bp in the case study to speed up vignette build.

--- R/plotPatterns.R
+++ R/plotPatterns.R
@@ -9,8 +9,8 @@
 #' @param colors an optional argument that specifies the colors for each differential combinatorial pattern
 #'
 #' @return A pheatmat
-#' 
-#' @examples 
+#'
+#' @examples
 #' # Creating dummy object
 #' countData <- cbind(rbind(matrix(rnbinom(1e2, mu = 1, size = 10), ncol = 1),
 #'                          matrix(rnbinom(1e2, mu = 10, size = 5), ncol = 1),
@@ -26,12 +26,12 @@
 #'                          matrix(rnbinom(1e2, mu = 1, size = 10), ncol = 1),
 #'                          matrix(rnbinom(1e2, mu = 10, size = 5), ncol = 1),
 #'                          matrix(rnbinom(1e2, mu = 1, size = 10), ncol = 1)))
-#' 
+#'
 #' colData <- data.frame(condition = c('A','B'), replicate = c(1,1))
 #' rowRanges <- GenomicRanges::GRanges('chrA',
 #'                      IRanges::IRanges(start = seq(1,by = 500,
 #'                      length.out = nrow(countData)),width = 500))
-#' 
+#'
 #' object <- epigraHMMDataSetFromMatrix(countData,colData,rowRanges = rowRanges)
 #'
 #' # Initializing
@@ -44,7 +44,7 @@
 #' peaks <- callPeaks(object = object,
 #'                   hdf5 = S4Vectors::metadata(object)$output,
 #'                   method = 'viterbi')
-#' 
+#'
 #' # Plotting patterns
 #' plotPatterns(object,
 #'              ranges = peaks[1],
@@ -65,10 +65,10 @@ plotPatterns = function(object,
 
     # Checking if differential analysis
     if (!(length(unique(object$condition)) > 1)) stop('Only one condition is present in the object input.')
-    
+
     # Checking if hdf5 file exists
     if(!file.exists(hdf5)) stop('The hdf5 file does not exist.')
-    
+
     # Reading in posterior probabilties and patterns
     postprob <- rhdf5::h5read(hdf5,'mixtureProb')
     patterns <- getPatterns(x = object,hdf5 = hdf5)
@@ -76,21 +76,21 @@ plotPatterns = function(object,
     # Subsetting
     sub_index <- if (methods::is(ranges)[1] == "GRanges") overlapsAny(object, ranges) else seq(ranges[1], ranges[2])
     sub_object <- object[sub_index,]
-    sub_postprob <- postprob[sub_index,]
+    sub_postprob <- postprob[sub_index,,drop = FALSE]
     colnames(sub_postprob) <- patterns
-    
+
     # Checking overlaps with differential peaks
     diff_index <- overlapsAny(sub_object, peaks)
     sub_postprob[!diff_index,] <- NA
-    
-    # Annotation 
+
+    # Annotation
     if(is.null(colors)) colors <- grDevices::palette.colors(n = 10, "Tableau 10")
-    
+
     if(length(colors)<ncol(sub_postprob)) stop('Insufficient number of colors. Please provide ',ncol(sub_postprob),' different colors.')
-        
+
     anno_colors <- list('Pattern' = colors[seq_len(ncol(sub_postprob))])
     names(anno_colors[['Pattern']]) <- colnames(sub_postprob)
-    
+
     # Plotting
     fig <- pheatmap::pheatmap(sub_postprob,
                        color = grDevices::colorRampPalette(c(4, "white", 2))(256),
@@ -103,6 +103,6 @@ plotPatterns = function(object,
                                      scales::comma(min(start(sub_object))),'-',
                                      scales::comma(max(end(sub_object))),')'),
                        labels_row = 'Genomic\nWindows',annotation_names_col = FALSE)
-    
+
     return(fig)
 }
